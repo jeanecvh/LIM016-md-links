dc8707cbc7d02259ee0e18e40a8b080dac4fd5c8
jeanellach <jeanellach@gmail.com>
2021-12-08 20:53:37 -0500

    primera configuración package.son

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+let fs = require('fs');
+import { existsSync } from 'fs';
+const path = require('path');
+/*
 module.exports = () => {
   // ...
 };
+*/
+
+const filesDirectory = fs.readdirSync('./');
+console.log(filesDirectory);
+
+if (existsSync('/etc/passwd'))
+  console.log('The path exists.');
+
+
