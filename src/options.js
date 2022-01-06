7ce671a261da447a486085ba5ed6a6a35ed81711
jeanellach <jeanellach@gmail.com>
2022-01-05 22:00:59 -0500

    flujograma CLI

--- src/options.js
+++ src/options.js
@@ -16,18 +16,25 @@ const brokenOfLinksStats = (arrayOfLinks)=> {
 }
 
 const userHelper = ()=> {
+
   return `
   ***************************************** MD-LINKS HELP ***************************************************
-    Debe ingresar la direccion del archivo ya sea relativa o absoluta, por ejemplo:
+    Debe ingresar la ruta del archivo ya sea relativa o absoluta, por ejemplo:
       md-links C:/Users/Jeanella/Desktop/LIM016-md-links/carpeta_de_prueba o
       md-links carpeta_de_prueba
     Opciones validas:
       --stats
+          Ejemplo:
+            md-links carpeta_de_prueba --stats
+            Total: 3
+            Unique: 3
       --validate
+
       --stats --validate  o --validate --stats
-    Ejemplos:
-      md-links src --stats
-      md-links D:\\Laboratoria\\LIM015-md-links\\src\\anotherExample\\example2.md --validate --stats`
+          Ejemplo:
+            md-links carpeta_de_prueba --stats --validate
+            Total: 3
+            Unique: 3`
   }
 
 
