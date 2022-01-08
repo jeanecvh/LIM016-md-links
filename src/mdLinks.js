8df288508955b18bf9fa4ddd1e7b8e9a82051ad9
jeanellach <jeanellach@gmail.com>
2022-01-08 00:41:45 -0500

    mdLinks function

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,3 +1,19 @@
 const allFunctions = require('./api.js')
 
-const mdLinks = (path, option)
+const mdLinks = (path, option = {}) => {
+  return new Promise ((resolve, reject) => {
+    if (!allFunctions.routeExists(path)){
+      reject("La ruta no existe");
+    } else {
+      const propiedadesDeLinks =  allFunctions.obtenerLinks(path);
+        if (!(option.validate)){
+          resolve (propiedadesDeLinks);
+        }
+    }
+  });
+}
+
+ const resultado = mdLinks('C:/Users/Jeanella/Desktop/LIM016-md-links/carpeta_de_prueba', { validate: false })
+resultado.then((resul) => console.log (resul)).catch((err)=> console.log(err));
+
+module.exports = { mdLinks };
